cf839070e1018cd5243ea86b45831b7d5e097cf8
Rafik Moreira <contato@rafikmoreira.dev.br>
2023-10-22 16:15:12 -0300

    add cobra cli

---
+++ cmd/grpc.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2023 Rafik Moreira <contato@rafikmoreira.dev.br>
+*/
+package cmd
+
+import (
+	"github.com/rafikmoreira/codepix/application/grpc"
+	"github.com/rafikmoreira/codepix/infrastructure/db"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var portNumber int
+
+// grpcCmd represents the grpc command
+var grpcCmd = &cobra.Command{
+	Use:   "grpc",
+	Short: "Start gRPC server",
+	Long:  `Start gRPC server to start CodePix gRPC server.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.ConnectDB(os.Getenv("env"))
+		grpc.StartGrpcServer(database, portNumber)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(grpcCmd)
+
+	grpcCmd.Flags().IntVarP(&portNumber, "port", "p", 50051, "gRPC Server port")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// grpcCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// grpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Rafik Moreira <contato@rafikmoreira.dev.br>
+*/
+package main
+
+import "github.com/rafikmoreira/codepix/cmd"
+
+func main() {
+	cmd.Execute()
+}
